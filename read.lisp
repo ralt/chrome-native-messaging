aefc3ed60764dc2d58145bb0e6707a0133868e06
Florian MARGAINE <florian@margaine.com>
2015-01-17 02:26:40 +0100

    Fixes a bug where it didn't read correctly.

--- read.lisp
+++ read.lisp
@@ -2,11 +2,8 @@
 
 (defun read-from-ext (stream)
   "Reads the string sent from the extension. The stream is usually *standard-input*."
-  (handler-case
-      (let ((length (read-length stream)))
-	(read-stdin-as-string stream length))
-    ; When no byte is available, it means the communication is cut off.
-    (end-of-file () nil)))
+  (let ((length (read-length stream)))
+    (read-stdin-as-string stream length)))
 
 (defun read-stdin-as-string (stream length)
   "Helper function to read *standard-input* as a string"
